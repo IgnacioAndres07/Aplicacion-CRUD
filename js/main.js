a31d973d8cf3ab92e97138b3b524dbc1e9db9744
Ignacio Andrés Gómez Díaz <ignacioandres@MacBook-Pro-de-Ignacio.local>
2023-08-10 15:48:57 -0400

    Me encuentro trabajando en javascript dando logica al ingresa un nuevo servicio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+// Creamos un array para la lista de servicios 
+
 let listaservicios = [];
 
+const objServicio = {
+    id: '',
+    servicio: '',
+    marca: '',
+    modelo: '',
+    Año: ''
+}
 
+// Let variable que nos va permitir cuando tenemos que agregar información
+let editando = false;
+
+const formulario = document.querySelector('#formulario');
+const servicioInput = document.querySelector('#servicio');
+const marcaInput = document.querySelector('#marca');
+const modeloInput = document.querySelector('#modelo');
+const añoInput = document.querySelector('#año');
+const btnAgregarInput = document.querySelector('#btnAgregar');
+
+// Creación de función cuando detecte el sumbit para validar formulario
+formulario.addEventListener('submit', validarFormulario);
+
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    if(servicioInput.value === '' || marcaInput.value === ''|| modeloInput.value === '' || añoInput.value === ''  ) {
+        alert('Todos los campos se deben llenar');
+        return;
+    }
+
+    if(editando) {
+        editarServicio();
+        editando = false;
+    } else {
+        objServicio.id = Date.now();
+        objServicio.servicio = servicioInput.value;
+        objServicio.marca = marcaInput.value;
+        objServicio.modelo = modeloInput.value;
+        objServicio.año = añoInput.value;
+
+        agregarEmpleado();
+    }
+}
